d3fc67d5d923ea0da82040573297c39f984b3e0e
bárbara miranda <barbaramntmiranda@gmail.com>
2024-03-08 12:31:39 -0300

    fix bugs and clean up code

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 let returnArea = '';
+const textarea = document.getElementById('inputText');
+
+const containerMessage = document.getElementById('container_message');
+const originalContent = containerMessage.innerHTML;
+
+const encryptButton = document.getElementById('encrypt-button');
+const decryptButton = document.getElementById('decrypt-button');
+const copyButton = document.getElementById('copy-button');
+
+
+textarea.addEventListener('input', inputValidation);
 
 function inputValidation() {
-    let textarea = document.getElementById('inputText');
     input = textarea.value;
     const regex = /^[a-zA-Z0-9\s]+$/;
-    let copyButton = document.getElementById('copy-button');
-    
 
     if (input.length === 0) {
-        document.getElementById('encrypt-button').setAttribute('disabled', true);
-        document.getElementById('decrypt-button').setAttribute('disabled', true);
-        copyButton.innerHTML = 'Copiar';
-        
+        encryptButton.setAttribute('disabled', true)
+        decryptButton.setAttribute('disabled', true);
+        copyButton.setAttribute('hidden', true);
+        containerMessage.innerHTML = originalContent;    
+
         return;
-    } else {
-        document.getElementById('encrypt-button').removeAttribute('disabled');
-        document.getElementById('decrypt-button').removeAttribute('disabled');
+
     }
+
+    encryptButton.removeAttribute('disabled');
+    decryptButton.removeAttribute('disabled');
 
     if (!regex.test(input)) {
         alert("Por favor, insira apenas letras e números.");
@@ -25,11 +35,11 @@ function inputValidation() {
 }
 
 function encrypt() {
-    let inputText = document.getElementById('inputText').value;
-    document.getElementById('copy-button').removeAttribute('hidden');
+    let inputText = textarea.value;
+    copyButton.removeAttribute('hidden');
 
-    if (inputText.length != 0) {
-        inputText = inputText
+    if (inputText.length !== 0) {
+        let encryptedText = inputText
             .replace(/e/g, 'enter')
             .replace(/i/g, 'imes')
             .replace(/a/g, 'ai')
@@ -37,17 +47,17 @@ function encrypt() {
             .replace(/u/g, 'ufat');
 
         let displayElement = document.getElementById('container_message');
-        displayElement.innerHTML = inputText;
-        returnArea = inputText;
-    } else {}
+        displayElement.innerHTML = encryptedText;
+        returnArea = encryptedText;
+    } else { }
 }
 
 function decrypt() {
-    let inputText = document.getElementById('inputText').value;
-    document.getElementById('copy-button').removeAttribute('hidden');
+    let inputText = textarea.value;
+    copyButton.removeAttribute('hidden');
 
-    if (inputText.length != 0) {
-        inputText = inputText
+    if (inputText.length !== 0) {
+        let decryptedText = inputText
             .replace(/enter/g, 'e')
             .replace(/imes/g, 'i')
             .replace(/ai/g, 'a')
@@ -55,8 +65,8 @@ function decrypt() {
             .replace(/ufat/g, 'u');
 
         let displayElement = document.getElementById('container_message');
-        displayElement.innerHTML = inputText;
-        returnArea = inputText;
+        displayElement.innerHTML = decryptedText;
+        returnArea = decryptedText;
     }
 }
 
@@ -65,16 +75,11 @@ function copyingText() {
     let copyArea = document.createElement('textarea');
     copyArea.value = copyText;
     document.body.appendChild(copyArea);
+
     copyArea.select();
     navigator.clipboard.writeText(copyArea.value)
         .then(function () {
             document.body.removeChild(copyArea);
-
-            let copyButton = document.getElementById('copy-button');
             copyButton.innerHTML = 'Copiado!'
         });
 }
-
-
-
-
